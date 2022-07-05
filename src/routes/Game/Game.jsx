405a63172eb551fefe0b7f3337f7dffeefc72aca
hernaninsaurralde <insaurraldehernan@gmail.com>
2022-07-05 18:49:35 -0300

    pokedex css

--- src/routes/Game/Game.jsx
+++ src/routes/Game/Game.jsx
@@ -6,105 +6,139 @@ import { Link } from 'react-router-dom';
 import { useMemo } from 'react';
 import pokedex from '/images/pokedex.png';
 import Loading from '/images/pokeLoading.gif';
-
+import gamelogo from '/images/gamelogo.png';
+import gamegif from '/images/gamegif.gif';
+import background from '/images/pokemongame.png';
+import Navbar from '../../components/Navbar/Navbar';
+import reload from '/images/reload.svg';
 
 export default function Game() {
 
     const [input, setInput] = useState('')
-    const [success, setSuccess] = useState(false)
+    const [success, setSuccess] = useState(true)
     const [game, setGame] = useState(false)
-    const [allPokemons1, setAllPokemons1] = useState([]);
+    const [allPokemons, setAllPokemons] = useState([]);
     const [chosenPokemons, setChosenPokemons] = useState([]);
-    const baseURL = 'https://pokeapi.co/api/v2/pokemon/?limit=100';
+    const [pokeOptions, setPokeOptions] = useState([]);
+    const baseURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     //const [pokeOptions, setPokeOptions] = useState([])
     // console.log(myPokemon[0] ? myPokemon[0].name : 'no hay nombre')
 
 
-    useEffect(() => {
-       
-        const grabData = async () => {
-            const { data } = await axios(baseURL)
-            const { results } = data
-            return Promise.all(
-                results.map(async (pokeData) => {
-                    const pokemon = await axios(pokeData.url);
-                    return pokemon
-                })
-            )
+    const actualizar = () => {
+
+
+        let pokeSorted = []
+        console.log("all pk",allPokemons)
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1) + min);
         }
-        grabData()
-            .then(data => {
-                 console.log('tiene que entea 1 vez')
-                setAllPokemons1(data)
-                setChosenPokemons(allPokemons1)
-            })
-    }, [])
+
+        for(let i = 0; i < 4; i++){
+            //chosenPokemons.length
+            let indexRandom = getRandomInt(0, 148) ;
+
+            //let poke = chosenPokemons.filter(indexRandom, 1);
+            
+            const { 0: poke } = allPokemons.filter((poke) => poke.data.id === indexRandom);
+            
+            pokeSorted.push(poke)
+            
+        }
+            console.log(pokeSorted)
+        setPokeOptions(pokeSorted)
+}
 
 
-    console.log("allpokemon", allPokemons1)
 
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min);
+useEffect(() => {
+    console.log("entro al useEffect")
+    
+    if(!allPokemons.length){
+        console.log("entro al 1er if")
+        
+                const grabData = async () => {
+                    const { data } = await axios(baseURL)
+                    const { results } = data
+                    return Promise.all(
+                        results.map(async (pokeData) => {
+                            const pokemon = await axios(pokeData.url);
+                            return pokemon
+                        })
+                    )
+                }
+                grabData()
+                        .then(data => {
+                            console.log('tiene que entea 1 vez')
+                            setAllPokemons(data)
+                            setChosenPokemons(prevState => [prevState, ...allPokemons])
+                        })
+    }
+        
+    if(!pokeOptions.length && allPokemons.length) {
+        console.log("entro al 2do if")
+                actualizar()
+           
+
     }
 
-    /*
-    useEffect(() => {
-        console.log('entra use effect')
-        
-            setPokeOptions([])
-            for (let i = 0; i < 4; i++) {
-                //chosenPokemons.length
-                let indexRandom = getRandomInt(0, 10);
-                let poke = chosenPokemons.splice(indexRandom, 1);
-                console.log("poke", poke)
-                setPokeOptions(prevState => [...prevState, ...poke])
-            }
-        console.log(pokeOptions)
-    }, [chosenPokemons]) */
+}, [allPokemons ])
 
-    
-         const pokeOptions = useMemo(()=>{
-            let pokeOptions = []
-            console.log("all pk",allPokemons1)
 
-            for(let i = 0; i < 4; i++){
-                //chosenPokemons.length
-                let indexRandom = getRandomInt(0, 10) ;
-                let poke = chosenPokemons.splice(indexRandom, 1);
-                console.log("poke", poke)
-                pokeOptions.push(...poke)
-        }
-            return pokeOptions
-         },[allPokemons1])
-    
-         console.log(pokeOptions)
-         
+
+  console.log(pokeOptions)
 
     return (
         <div className={style.game}>
 
+            
             {pokeOptions.length === 0 ? (
                 <h3>
                     <img src={Loading} className={style.loading} alt="Loading..." />
                 </h3>
             ) : (
-                <>
-                    <img className={style.pokedex} src={pokedex} alt="pokedex">
-                        
-                    </img>
-                    <div className={style.options}>
+                
+
+    <div className={style.game}>
+            <Link to='/home' style={{textDecoration: 'none'}} className={style.home}><button className={style.button}><img  alt="pokebola" width='20px'/> Home</button></Link>
+            <img src={gamelogo} className={style.logo}alt="pokebola" width='540px'/>
+            
+            <div className={style.header}>
+              <button  onClick={(e) => handleReload(e)} className={style.reload}><img src={reload} alt="pokebola" width='40px'/></button>
+              <img src={background} className={style.bkg} alt="pokebola" width='580px'/>
+              {
+                <img src={gamegif} style={ game ? {filter: 'grayscale(0) brightness(100%)'} : {}} className={style.img} alt="Pokemon" width='220px'/> 
+            
+              }
+              <span className={style.introduction}>Test your knowledge with this guess Pokemon game!
+              
+              <div className={style.options}>
                         <button > {pokeOptions[0].data.name} </button>
                         <button > {pokeOptions[1].data.name} </button>
                         <button > {pokeOptions[2].data.name} </button>
                         <button > {pokeOptions[3].data.name} </button>
-                    </div>
-                    
-                </>
-
-
-
+                    </div></span>
+            
+              {
+                  game ? 
+                  success ?
+                  <div className={style.result}>
+                      Correct! This pokemon is 
+                  </div> :
+                  <div className={style.result}>
+                      Oops! Incorrect. This pokemon is
+                  </div>
+                  :
+                  <div></div>
+              }
+            </div>
+        </div>
+               
+        
+                
+                
 
             )
 
