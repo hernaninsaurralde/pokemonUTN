eaeb40d631197eeaee28f3300138f905523b51a1
hernaninsaurralde <insaurraldehernan@gmail.com>
2022-07-14 18:17:36 -0300

    pack json4

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react", // ignore react stuff
-        "react-dom",
-      ],
-    },
-  },
+  plugins: [react()]
 })
